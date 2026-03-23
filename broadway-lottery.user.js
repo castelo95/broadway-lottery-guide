7b5cd9be28b8d009be373a5fa4067d3eed9cd5a6
Javier Castello <jcastello@uc.cl>
2026-03-23 01:06:11 -0400

    bump: v13.2

--- broadway-lottery.user.js
+++ broadway-lottery.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Broadway Lottery 🎭
 // @namespace    https://bwayrush.com/
-// @version      13.1
+// @version      13.2
 // @description  Broadway Lottery Autopilot — Broadway Direct, Lucky Seat, Telecharge (coming soon)
 // @author       Javier Castello
 // @updateURL    https://castelo95.github.io/broadway-lottery-guide/broadway-lottery.user.js
